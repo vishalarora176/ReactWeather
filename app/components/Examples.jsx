e5fb0e0360d3a2ee2e3227dfd00f149899995c9f
Arora <vishal.arora03@sap.com>
2017-10-16 10:22:12 +0530

    modified the exaples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,10 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-        <h3>Examples Component</h3>
+        <div>
+            <h3>Examples Component</h3>
+            <p>This is a simple page to show examples!</p>
+        </div>
     );
 };
 
